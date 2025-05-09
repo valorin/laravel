77e31c0b4457375f7d68065c2510e3396ebc998f
Stephen Rees-Carter <stephen@rees-carter.net>
2025-05-10 00:08:17 +1000

    Add more commands for testing other methods

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,13 @@
 <?php
 
+use Illuminate\Auth\Passwords\PasswordBroker;
+use Illuminate\Auth\Passwords\TokenRepositoryInterface;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Benchmark;
 use Illuminate\Support\Facades\Artisan;
 use \App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Password;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
@@ -67,4 +70,114 @@ Artisan::command('timing-attack:attempt {iterations}', function ($iterations) {
         },
     ], $iterations);
 
+});
+
+Artisan::command('timing-attack:sendResetLink {iterations}', function ($iterations) {
+
+    $user = User::firstOrCreate([
+        'email' => 'stephen@valorinsecurity.com',
+    ], [
+        'name' => 'Stephen',
+        'password' => bcrypt('password'),
+    ]);
+
+    $tokens = new class implements TokenRepositoryInterface
+    {
+        public function create(\Illuminate\Contracts\Auth\CanResetPassword $user)
+        {
+            return false;
+        }
+
+        public function exists(\Illuminate\Contracts\Auth\CanResetPassword $user, $token)
+        {
+            return false;
+        }
+
+        public function recentlyCreatedToken(\Illuminate\Contracts\Auth\CanResetPassword $user)
+        {
+            return false;
+        }
+
+        public function delete(\Illuminate\Contracts\Auth\CanResetPassword $user)
+        {
+            return false;
+        }
+
+        public function deleteExpired()
+        {
+            return false;
+        }
+    };
+
+    $broker = new PasswordBroker(
+        $tokens,
+        Auth::createUserProvider('users'),
+        app('events'),
+        //timeboxDuration: config('auth.timebox_duration'),
+    );
+
+    Benchmark::dd([
+        'Known Email' => function () use ($user, $broker) {
+            return $broker->sendResetLink(['email' => $user->email]);
+        },
+        'Unknown Email' => function () use ($broker) {
+            return $broker->sendResetLink(['email' => 'noone@test.com']);
+        },
+    ], $iterations);
+});
+
+Artisan::command('timing-attack:reset {iterations}', function ($iterations) {
+
+    $user = User::firstOrCreate([
+        'email' => 'stephen@valorinsecurity.com',
+    ], [
+        'name' => 'Stephen',
+        'password' => bcrypt('password'),
+    ]);
+
+    $tokens = new class implements TokenRepositoryInterface
+    {
+        public function create(\Illuminate\Contracts\Auth\CanResetPassword $user)
+        {
+            return false;
+        }
+
+        public function exists(\Illuminate\Contracts\Auth\CanResetPassword $user, $token)
+        {
+            return false;
+        }
+
+        public function recentlyCreatedToken(\Illuminate\Contracts\Auth\CanResetPassword $user)
+        {
+            return false;
+        }
+
+        public function delete(\Illuminate\Contracts\Auth\CanResetPassword $user)
+        {
+            return false;
+        }
+
+        public function deleteExpired()
+        {
+            return false;
+        }
+    };
+
+    $broker = new PasswordBroker(
+        $tokens,
+        Auth::createUserProvider('users'),
+        app('events'),
+        //timeboxDuration: config('auth.timebox_duration'),
+    );
+
+    $callback = fn () => null;
+
+    Benchmark::dd([
+        'Known Email' => function () use ($user, $broker, $callback) {
+            return $broker->reset(['email' => $user->email, 'token' => 'abc123'], $callback);
+        },
+        'Unknown Email' => function () use ($broker, $callback) {
+            return $broker->reset(['email' => 'noone@test.com', 'token' => 'abc123'], $callback);
+        },
+    ], $iterations);
 });
